cccc78ddf35838ef243591b6c882b5d5dc214981
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2019-11-24 06:51:53 +0800

    Add files via upload

---
+++ lol/config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyD3Xq0ILCTlEzrEF9N1VjDRDg01y7pCEYQ');
+define('GJ_CODE', 'AU');
+define('SITE_NAME', '小猫屋');
+define('TITLENAME', '小猫屋');
+define('EN2DEKEY', 'VvJ01XdKho');
+define('EMAIL', '1137254268@qq.com');
+?>
